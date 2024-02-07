502d8529d456a0b37bb8cc8c627dc5c9b433bb5a
SEONGEUN, KIM <manuma159159@gmail.com>
2024-02-07 10:14:54 +0900

    게시판 -프론트엔드-

--- app/routes/board.py
+++ app/routes/board.py
@@ -1,17 +1,26 @@
+from fastapi import Request
+
 from fastapi import APIRouter
+
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 board_router = APIRouter()
 
-@board_router.get('/list')
-def list():
-    return {'msg':'Hello, Board list!'}
+templates = Jinja2Templates(directory='views/templates')
+board_router.mount('/static',StaticFiles(directory='views/static'), name='static')
+
+@board_router.get('/list', response_class=HTMLResponse)
+def list(req: Request):
+    return templates.TemplateResponse('board/list.html',{'request':req})
 
 
-@board_router.get('/write')
-def write():
-    return {'msg':'Hello, Board write!'}
+@board_router.get('/write', response_class=HTMLResponse)
+def write(req: Request):
+    return templates.TemplateResponse('board/write.html',{'request':req})
 
 
-@board_router.get('/view')
-def view():
-    return {'msg':'Hello, Board view!'}
+@board_router.get('/view', response_class=HTMLResponse)
+def view(req: Request):
+    return templates.TemplateResponse('board/view.html',{'request':req})
