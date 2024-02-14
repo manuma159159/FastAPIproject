fbaad512e00d2040e9379082db1e9a1a31c5bf3b
SEONGEUN, KIM <manuma159159@gmail.com>
2024-02-14 11:44:06 +0900

    갤러리 1 -프론트엔드-

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fastapi.staticfiles import StaticFiles
 
 from app.dbfactory import db_startup
 from app.routes.board import board_router
+from app.routes.gallery import gallery_router
 from app.routes.member import member_router
 
 
@@ -25,6 +26,7 @@ app.mount('/static',StaticFiles(directory='views/static'), name='static')
 # 외부 route 파일 불러오기
 app.include_router(member_router)
 app.include_router(board_router, prefix='/board')
+app.include_router(gallery_router, prefix='/gallery')
 
 # 서버 시작시 디비 생성
 
